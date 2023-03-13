a91e92c5633c43f0022cb461788538307a5b896c
LucasAlencar <lucas.linard@outlook.com>
2023-03-13 11:35:07 -0300

    new: theme for app

---
+++ src/theme/index.ts
@@ -0,0 +1,20 @@
+const theme = {
+    BASE:{
+        GRAY100: '#F2F2F2',
+        GRAY200: '#D9D9D9',
+        GRAY300: '#808080',
+        GRAY400: '#333333',
+        GRAY500: '#262626',
+        GRAY600: '#1A1A1A',
+        GRAY700: '#0D0D0D',
+    },
+    PRODUTO: {
+        PURPLE: '#8284FA',
+        PURPLE_DARK: '#5E60CE',
+        BLUE: '#4EA8DE',
+        BLUE_DARK: '#1E6F9F',
+        DANGER: '#E25858'
+    }
+}
+
+export default theme;
