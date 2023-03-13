f88b9255c7dfe710909524ff22cc60a548d7d1eb
LucasAlencar <lucas.linard@outlook.com>
2023-03-13 14:51:25 -0300

    feature: style changed if is onFocus

--- src/components/NewTask/index.tsx
+++ src/components/NewTask/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, TextInput, Text } from "react-native";
 import { SquareButton } from "../SquareButton";
 
@@ -5,12 +6,24 @@ import { styles } from "./styles";
 import theme from "../../theme";
 
 export function NewTask() {
+    const [ isFocused, setIsFocused ] = useState(false);
+
+    const handleInputFocus = () => {
+        setIsFocused(true);
+    }
+
+    const handleInputBlur = () => {
+        setIsFocused(false);
+    }
+
     return(
         <View style={styles.container}>
             <TextInput 
-            style={styles.textInput}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            style={isFocused ? styles.textInputFocused : styles.textInput}
             placeholder="Adicionar uma nova tarefa"
-            placeholderTextColor={theme.BASE.GRAY100}
+            placeholderTextColor={isFocused? theme.BASE.GRAY100 : theme.BASE.GRAY300 }
             />
             <SquareButton/>
         </View>
