1a833fcdf7ec42fe0175ec5e1271e6e1890f5c27
LucasAlencar <lucas.linard@outlook.com>
2023-03-15 10:08:09 -0300

    Touchable props added

--- src/components/SquareButton/index.tsx
+++ src/components/SquareButton/index.tsx
@@ -1,14 +1,11 @@
-import { TouchableHighlight } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import { TouchableHighlight, TouchableHighlightProps } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { styles } from "./styles";
 
-
-
-
-export function SquareButton() {
-    return(
-        <TouchableHighlight style={styles.container}>
-            <Ionicons name="add-circle-outline" size={24} color="white" />
-        </TouchableHighlight>
-    )
+export function SquareButton(props: TouchableHighlightProps) {
+  return (
+    <TouchableHighlight onPress={props.onPress} style={styles.container}>
+      <Ionicons name="add-circle-outline" size={24} color="white" />
+    </TouchableHighlight>
+  );
 }
