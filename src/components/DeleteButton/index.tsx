8fd951255d5a2b6e5369efb0c313854b8e0f6603
LucasAlencar <lucas.linard@outlook.com>
2023-03-15 10:06:19 -0300

    DeleteButton Touchable props added

--- src/components/DeleteButton/index.tsx
+++ src/components/DeleteButton/index.tsx
@@ -1,10 +1,10 @@
 import  { useState } from 'react';
-import { TouchableHighlight } from "react-native";
+import { TouchableHighlight, TouchableHighlightProps  } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import theme from "../../theme";
 import { styles } from "./styles";
 
-export function DeleteButton() {
+export function DeleteButton(props: TouchableHighlightProps) {
     const [isPressed, setIsPressed] = useState(false);
 
   const iconColor = isPressed ? theme.PRODUTO.DANGER : theme.BASE.GRAY300;
@@ -14,6 +14,7 @@ export function DeleteButton() {
     underlayColor={theme.BASE.GRAY400}
     onPressIn={() => setIsPressed(true)}
     onPressOut={() => setIsPressed(false)}
+    onPress={props.onPress}
     style={styles.container}>
       <Ionicons name="trash-outline" size={17} color={iconColor} />
     </TouchableHighlight>
