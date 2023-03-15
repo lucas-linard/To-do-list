25a6fa0bc45af3daf553f2b179c67cc89e675893
LucasAlencar <lucas.linard@outlook.com>
2023-03-15 10:59:34 -0300

    underlayColor added

--- src/components/SquareButton/index.tsx
+++ src/components/SquareButton/index.tsx
@@ -1,10 +1,11 @@
 import { TouchableHighlight, TouchableHighlightProps } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import theme from "../../theme";
 import { styles } from "./styles";
 
 export function SquareButton(props: TouchableHighlightProps) {
   return (
-    <TouchableHighlight onPress={props.onPress} style={styles.container}>
+    <TouchableHighlight underlayColor={theme.PRODUTO.BLUE} onPress={props.onPress} style={styles.container}>
       <Ionicons name="add-circle-outline" size={24} color="white" />
     </TouchableHighlight>
   );
