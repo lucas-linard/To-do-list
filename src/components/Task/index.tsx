ddc28b5f334b5d8337703e0581661ba585deeb77
LucasAlencar <lucas.linard@outlook.com>
2023-03-15 10:10:43 -0300

    Component reactive

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, Text } from "react-native";
 
 import { CheckButton } from "../CheckButton";
@@ -5,12 +6,19 @@ import { DeleteButton } from "../DeleteButton";
 
 import { styles } from "./styles";
 
-export function Task() {
+type Props ={ 
+  text: string;
+  isDone: boolean;
+  onDelete: () => void;
+}
+
+export function Task(props: Props) {
+  const [isDone, setIsDone] = useState(props.isDone);
   return (
     <View style={styles.container}>
-      <CheckButton isDone={false}/>
-      <Text style={styles.text}>This library is installed by defalut by then expo start</Text>
-      <DeleteButton/>
+      <CheckButton onPress={() => setIsDone(!isDone)} isDone={isDone}/>
+      <Text style={isDone ? styles.textDone : styles.text}>{props.text}</Text>
+      <DeleteButton onPress={props.onDelete}/>
     </View>
   );
 }
