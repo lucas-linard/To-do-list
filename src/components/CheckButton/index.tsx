67ca400034d3472b298e72cdb56f34b2928f8633
LucasAlencar <lucas.linard@outlook.com>
2023-03-15 10:04:52 -0300

    CheckButton Touchable Props and change styles

--- src/components/CheckButton/index.tsx
+++ src/components/CheckButton/index.tsx
@@ -1,17 +1,21 @@
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { styles } from "./styles";
 
 
 import theme from "../../theme";
 
-type Props = {
+interface Props extends TouchableOpacityProps {
     isDone: boolean;
 }
-export function CheckButton(props: Props) {
+
+export function CheckButton({ isDone, ...props }: Props) {
     return (
-        <TouchableOpacity style={props.isDone ? styles.containerDone : styles.container}>
-        {props.isDone && <Ionicons name="checkmark-sharp" size={12} color={theme.BASE.GRAY100} />}  
+        <TouchableOpacity 
+        onPress={props.onPress}
+        style={isDone ? styles.containerDone : styles.container}
+        >
+        {isDone && <Ionicons name="checkmark-sharp" size={12} color={theme.BASE.GRAY100} />}  
         </TouchableOpacity>
     );
 }
