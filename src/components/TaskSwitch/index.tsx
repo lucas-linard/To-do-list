d4e850777a0d155a40530dc47d332556d21fc941
LucasAlencar <lucas.linard@outlook.com>
2023-03-16 11:10:14 -0300

    typed props and counter is now dynamic

--- src/components/TaskSwitch/index.tsx
+++ src/components/TaskSwitch/index.tsx
@@ -3,19 +3,24 @@ import { styles } from "./styles";
 import theme from "../../theme";
 import { Counter } from "../Counter";
 
+type Props = {
+  Created: number;
+  Completed: number;
+}
 
-export function TaskSwitch() {
+export function TaskSwitch(props: Props) {
     const textColor = theme.PRODUTO;
+
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.button}>
         <Text style={[styles.mainText,{color: textColor.BLUE}]}>Criadas</Text>
-        <Counter value={0}/>
+        <Counter value={props.Created}/>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.button}>
         <Text style={[styles.mainText,{color: textColor.PURPLE}]}>Concluidas</Text>
-        <Counter value={0}/>
+        <Counter value={props.Completed}/>
       </TouchableOpacity>
     </View>
   );
