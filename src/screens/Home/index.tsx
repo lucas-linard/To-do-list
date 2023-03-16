ed48f2a770ef43eeacade6a4f02ab4f0b1e2554a
LucasAlencar <lucas.linard@outlook.com>
2023-03-16 11:12:13 -0300

    Home now useStates and the components are functional

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,102 @@
-import { View,SafeAreaView,  Text } from "react-native";
+import { useState } from "react";
+import { View, FlatList } from "react-native";
 
-import {styles} from "./styles";
+import uuid from "react-native-uuid";
+import { styles } from "./styles";
 
 import { Header } from "../../components/Header";
+import { Task } from "../../components/Task";
+import { EmptyTask } from "../../components/EmptyTask";
 import { TaskInput } from "../../components/TaskInput";
 import { TaskSwitch } from "../../components/TaskSwitch";
+
+interface ITask {
+  id: string | number[];
+  text: string;
+  isDone: boolean;
+  onDelete?: () => void;
+}
+
 export default function Home() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [taskText, setTaskText] = useState<string>("");
+  const [counter, setCounter] = useState({ created: 0, completed: 0 });
 
-    return(
-        <SafeAreaView style={styles.mainContainer}>
-            <View style={styles.headerContainer}>
+  const handleAddTask = (text: string) => {
+    const newTask = {
+      id: uuid.v4(),
+      text,
+      isDone: false,
+      onDelete: () => {
+        if (newTask.isDone) {
+          setCounter((prevCounter) => ({
+            created: prevCounter.created - 1,
+            completed: prevCounter.completed - 1,
+          }));
+        } else if (!newTask.isDone) {
+          setCounter((prevCounter) => ({
+            created: prevCounter.created - 1,
+            completed: prevCounter.completed,
+          }));
+        }
+        setTasks((prevTasks) =>
+          prevTasks.filter((task) => task.id !== newTask.id)
+        );
+      },
+    };
 
-            <Header/>
+    setTasks([...tasks, newTask]);
+    setCounter((prevCounter) => ({
+      created: prevCounter.created + 1,
+      completed: prevCounter.completed,
+    }));
+    setTaskText("");
+  };
+
+  function handleFinishTask(item: ITask) {
+    let index = tasks.indexOf(item);
+    if (item.isDone) {
+      setCounter((prevCounter) => ({
+        created: prevCounter.created,
+        completed: prevCounter.completed - 1,
+      }));
+      tasks[index].isDone = false;
+    } else {
+      setCounter((prevCounter) => ({
+        created: prevCounter.created,
+        completed: prevCounter.completed + 1,
+      }));
+      tasks[index].isDone = true;
+    }
+  }
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.headerContainer}>
+        <Header />
+      </View>
+      <View style={styles.taskContainer}>
+        <TaskInput
+          text={taskText}
+          onChangeText={(text) => setTaskText(text)}
+          onAdd={() => handleAddTask(taskText)}
+        />
+        <TaskSwitch Created={counter.created} Completed={counter.completed} />
+        <FlatList
+          data={tasks}
+          renderItem={({ item }) => (
+            <View style={{ marginHorizontal: 19, marginBottom: 10 }}>
+              <Task
+                text={item.text}
+                isDone={item.isDone}
+                onDelete={item.onDelete}
+                onComplete={() => handleFinishTask(item)}
+              />
             </View>
-            <View style={styles.taskContainer}>
-            <TaskInput/>
-                <TaskSwitch/>
-            </View>
-        </SafeAreaView>
-    )
+          )}
+          ListEmptyComponent={<EmptyTask />}
+        />
+      </View>
+    </View>
+  );
 }
